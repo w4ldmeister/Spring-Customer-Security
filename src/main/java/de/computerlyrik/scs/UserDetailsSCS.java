859645e3b23b036e408047103a9debd85ab15adc
Christian Fischer <waldmeister23@googlemail.com>
2011-12-16 18:18:41 +0100

    initial commit of Version 0.9 for github

---
+++ src/main/java/de/computerlyrik/scs/UserDetailsSCS.java
@@ -0,0 +1,82 @@
+package de.computerlyrik.scs;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Transient;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.NotNull;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.MailSender;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import de.computerlyrik.scs.service.UserDetailsServiceImpl;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(finders = { "findUserDetailsSCSsByUsernameEquals" })
+public class UserDetailsSCS implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger log = Logger.getLogger(UserDetailsSCS.class);
+
+    @Autowired 
+	@Qualifier("userDetailsService")
+	@Transient
+	private UserDetailsServiceImpl uds;
+    
+	@NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+
+    private boolean accountNonExpired = true;
+
+    private boolean accountNonLocked = true;
+
+    private boolean credentialsNonExpired = true;
+
+    private boolean enabled = false;
+
+
+    public static de.computerlyrik.scs.UserDetailsSCS findMyUserByUsername(String username) {
+        if (username == null) return null;
+        log.info("call findMyUserByUsername");
+        TypedQuery<UserDetailsSCS> q = entityManager().createQuery("SELECT o FROM UserDetailsSCS AS o WHERE o.username = :username", UserDetailsSCS.class);
+
+        return entityManager().createQuery("SELECT o FROM UserDetailsSCS o WHERE o.username = '" + username + "'", UserDetailsSCS.class).getSingleResult();
+    }
+    
+    public List<org.springframework.security.core.GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> glist = new ArrayList<GrantedAuthority>();
+        log.info("call getAuthorities");
+        String roleString = "ROLE_" + this.getClass().getSimpleName().toUpperCase();
+        log.info("Adding " + roleString + " for User " + this.getUsername());
+        glist.add(new GrantedAuthorityImpl(roleString));
+        return glist;
+    }
+
+    public void setPassword(String password)  {
+    	try {
+    		this.password = uds.calcPassword(password);
+    	}
+    	catch(Exception e) {
+    		log.error("Failed setting password of "+this.username);
+    		log.info("Login of "+this.username+" disabled");
+    		this.password = "";
+    	}
+        log.info("Password of user " + username + " set to " + this.getPassword() +" (hash)");
+    }
+}
---
+++ src/main/java/de/computerlyrik/scs/service/UserDetailsServiceImpl.java
@@ -0,0 +1,56 @@
+package de.computerlyrik.scs.service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.persistence.TypedQuery;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import de.computerlyrik.scs.UserDetailsSCS;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+    private static final Logger log = Logger.getLogger(UserDetailsServiceImpl.class);
+
+	private String salt;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException 
+	{
+		//TypedQuery<UserDetailsSCS> q = 
+		//	UserDetailsSCS.findUserDetailsSCSsByUsernameEquals(username);
+		//UserDetailsSCS user = q.getSingleResult();
+		UserDetailsSCS user = UserDetailsSCS.findMyUserByUsername(username);
+		log.info("Got User "+user);
+
+		/*
+		 * if username not found:
+		 *       throw new UsernameNotFoundException(username + "not found");
+
+		 */
+		return (UserDetails) user;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	
+	public String calcPassword(String password) throws NoSuchAlgorithmException {
+	
+	    MessageDigest md = MessageDigest.getInstance("SHA-256");
+	    String salt = "mysecretsalt";
+	    byte[] hash = md.digest((password + "{" + salt + "}").getBytes());
+	    StringBuffer sb = new StringBuffer();
+	    for (byte b : hash) {
+	        sb.append(String.format("%02x", b));
+	    }
+	    return sb.toString();
+	}
+}
