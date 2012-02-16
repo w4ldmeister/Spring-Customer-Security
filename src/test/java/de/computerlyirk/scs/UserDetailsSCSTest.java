d1cfda75d49faa9f6c11b0882e58ea589887a21d
Christian Fischer <waldmeister23@googlemail.com>
2012-02-16 21:56:19 +0100

    added encoding tests for custom hashing and custom hashing+salting

--- src/test/java/de/computerlyirk/scs/UserDetailsSCSTest.java
+++ src/test/java/de/computerlyirk/scs/UserDetailsSCSTest.java
@@ -30,11 +30,18 @@ public class UserDetailsSCSTest {
 	@Qualifier("passwordEncoder")
 	private PasswordEncoderImpl pe;  
     
-	/**
+    @Autowired 
+	@Qualifier("hashingPasswordEncoder")
+	private PasswordEncoderImpl pe2;     
+	
+    @Autowired 
+	@Qualifier("saltedPasswordEncoder")
+	private PasswordEncoderImpl pe3; 
+    /**
 	 * Try out some preconfigured password/hash combinations
 	 */
 	@Test
-	public void passwordHashing() {
+	public void userDetailsSCSDefaultTest() {
 		Map<String,String> passwordhashes = new HashMap<String,String>();
 		passwordhashes.put("admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918");		
 		UserDetailsSCS u = new UserDetailsSCS();
@@ -43,6 +50,28 @@ public class UserDetailsSCSTest {
 			Assert.assertTrue(
 					"Password hashes should be equal!", 
 					u.getPassword()
+						.equals(passwordhashes.get(password)));
+		}
+	}	
+	
+	@Test
+	public void customHashingPasswordGeneration() {
+		Map<String,String> passwordhashes = new HashMap<String,String>();
+		passwordhashes.put("admin", "d033e22ae348aeb5660fc2140aec35850c4da997");		
+		for(String password : passwordhashes.keySet()) {			
+			Assert.assertTrue(
+					pe2.encodePassword(password,null)
+						.equals(passwordhashes.get(password)));
+		}
+	}
+	
+	@Test
+	public void customHashingSaltedPasswordGeneration() {
+		Map<String,String> passwordhashes = new HashMap<String,String>();
+		passwordhashes.put("admin", "f7011b38f770ec641e7629efe56c2dd1d181c70e");		
+		for(String password : passwordhashes.keySet()) {			
+			Assert.assertTrue(
+					pe3.encodePassword(password,null)
 						.equals(passwordhashes.get(password)));
 		}
 	}	
