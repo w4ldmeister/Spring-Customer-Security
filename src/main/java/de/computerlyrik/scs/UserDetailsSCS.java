8ac1ec12c668b306adfa27f1831ca6f318cc89a3
Christian Fischer <waldmeister23@googlemail.com>
2012-10-18 12:09:08 +0200

    added @MappedSuperclass to UserDetailsSCS

--- src/main/java/de/computerlyrik/scs/UserDetailsSCS.java
+++ src/main/java/de/computerlyrik/scs/UserDetailsSCS.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Transient;
 import javax.persistence.TypedQuery;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import de.computerlyrik.scs.service.UserDetailsServiceImpl;
 import org.springframework.security.authentication.encoding.PasswordEncoder;
 
+@MappedSuperclass
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(finders = { "findUserDetailsSCSsByUsernameEquals" })
